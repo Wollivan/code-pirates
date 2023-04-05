a03dd74ed850d4dcac12529317a5adbe9533737d
Tim Smith <tim.smith2814@gmail.com>
2023-04-05 19:28:29 +0100

    Update HornedBeast.js

--- class-01/lab/demo/src/Components/HornedBeast.js
+++ class-01/lab/demo/src/Components/HornedBeast.js
@@ -1,7 +1,9 @@
-function HornedBeast({ title }) {
+function HornedBeast({ title, imageUrl, description }) {
   return (
     <div className="horned-beast">
       <h2>{title}</h2>
+      <img src={imageUrl} alt={title} />
+      <p>{description}</p>
     </div>
   );
 }
